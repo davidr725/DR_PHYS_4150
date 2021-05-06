5666b60221c3fe47527c8ef7ca5d6c6bf89b6f51
davidr725 <78507338+davidr725@users.noreply.github.com>
2021-05-06 09:17:56 -0400

    Update Lab_10_CN.py
    updated comments, reorganized functions

--- Lab_10_CN.py
+++ Lab_10_CN.py
@@ -4,7 +4,12 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
+# Computational Functions
 def psi0(x):
+    '''
+    Returns the first value of psi at time = 0
+    for all given values of x
+    '''
     x0 = L/2
     sigma = 1e-10
     kappa = 5e10
@@ -14,9 +19,14 @@ def psi0(x):
     return psi_0
 
 
-def tridiag_matrix(A, a1, a2):
+def TriDiag_Matrix_Creator(A, a1, a2):
+    '''
+    Creates a Tridiag Matrix A
+    given diagonal element a1
+    and banded elements a2
+    '''
     for i in range(N):
-        A[i,i] = a1
+        A[i, i] = a1
         if i == 0:
             A[i, i+1] = a2
         elif i == N-1:
@@ -25,6 +35,22 @@ def tridiag_matrix(A, a1, a2):
             A[i, i+1] = a2
             A[i, i-1] = a2
     return A
+
+
+# Animation Functions
+def init():
+    line.set_data(xpoints, psi0(xpoints))
+    return line,
+
+
+def animate(i):
+    v = np.dot(MatrixB, psi[i])
+    psi.append(np.linalg.solve(MatrixA, v))
+    x = xpoints
+    y = psi[i]
+    i += 1
+    line.set_data(x, y)
+    return line,
 
 
 # Initial conditions
@@ -37,11 +63,11 @@ a = L/N
 t = 0
 t_end = 100000
 
-# Xpoints
+# Create X points
 xpoints = a*np.linspace(0, N, num=N+1, endpoint=False)
 xpoints = np.delete(xpoints, 0)  # delete the boundary condition
 
-# Matrix elements
+# Matrix elements for the TriDiag Function
 a1 = 1 + h*((1j*hbar)/(2*m*(a**2)))
 a2 = -h*((1j*hbar)/(4*m*(a**2)))
 b1 = 1 - h*((1j*hbar)/(2*m*(a**2)))
@@ -49,37 +75,19 @@ b2 = h*((1j*hbar)/(4*m*(a**2)))
 
 # Create Tridiagonal Matrices A & B
 MatrixA = np.zeros([N, N], dtype=complex)
-MatrixA = tridiag_matrix(MatrixA, a1, a2)
-
+MatrixA = TriDiag_Matrix_Creator(MatrixA, a1, a2)
 MatrixB = np.zeros([N, N], dtype=complex)
-MatrixB = tridiag_matrix(MatrixB, b1, b2)
+MatrixB = TriDiag_Matrix_Creator(MatrixB, b1, b2)
 
 # Animation
 psi = []
-psi.append(psi0(xpoints)) 
+psi.append(psi0(xpoints))
 fig = plt.figure()
 axis = plt.axes(xlim=(-1e-9, 11e-9), ylim=(-1, 1))
 axis.set_xlabel('x')
 axis.set_ylabel('$\psi(x)$')
 axis.set_title('Crank Nicolson Wave Function')
-line, = axis.plot([], [], lw = .5)
-
-
-def init():
-    line.set_data(xpoints, psi0(xpoints))
-    return line,
-
-
-def animate(i):
-    v = np.dot(MatrixB, psi[i])
-    psi.append(np.linalg.solve(MatrixA, v))
-    x = xpoints
-    y = psi[i]
-    i += 2500
-    line.set_data(x, y)
-
-    return line,
-
+line, = axis.plot([], [], lw=.5)
 
 ani = animation.FuncAnimation(
     fig, animate, init_func=init, frames=t_end, interval=0, blit=True,
